351e0dc7ef1ea153f2e5c28b88e68442b3f44a80
Robert Paruch <roobbb@live.com>
2020-06-02 22:40:59 -0700

    Edited Mobile App

--- S33_App_11-Mobile_App/main.py
+++ S33_App_11-Mobile_App/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -15,6 +16,17 @@ class SignUpScreen(Screen):
     def add_user(self, uname, pword):
         with open('users.json') as file:
             users = json.load(file)
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}
+        with open('users.json', 'w') as file:
+            json.dump(users, file)
+        self.manager.current = 'sign_up_screen_success'
+
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
 
 
 class RootWidget(ScreenManager):
