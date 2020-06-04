bfb163ca8cd3d77ef9d55c69cf2b52216e0cfacb
Robert Paruch <roobbb@live.com>
2020-06-04 03:15:46 -0700

    Edited Mobile App

--- S33_App_11-Mobile_App/main.py
+++ S33_App_11-Mobile_App/main.py
@@ -1,8 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json
 from datetime import datetime
+from glob import glob
+from pathlib import Path
+import random
+from hoverable import HoverBehavior
 
 Builder.load_file('design.kv')
 
@@ -10,6 +16,14 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = 'sign_up_screen'
+
+    def login(self, uname, pword):
+        with open('users.json') as file:
+            users = json.load(file)
+            if uname in users and users[uname]['password'] == pword:
+                self.manager.current = 'login_screen_success'
+            else:
+                self.ids.login_wrong.text = 'Wrong username or password!'
 
 
 class SignUpScreen(Screen):
@@ -27,6 +41,28 @@ class SignUpScreenSuccess(Screen):
     def go_to_login(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'login_screen'
+
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob('quotes/*.txt')
+        available_feelings = [Path(filename).stem
+                              for filename in available_feelings]
+        if feel in available_feelings:
+            with open(f'quotes/{feel}.txt', encoding='utf8') as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = 'Try another feeling!'
+
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 
 class RootWidget(ScreenManager):
